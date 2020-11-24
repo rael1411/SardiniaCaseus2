ecdc17cfc1301ec694c0a05eaaf7596f7e1b3d78
Alessio <barrialex@gmail.com>
2020-11-24 21:47:24 +0100

    reworked directory structure

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ function Product(url, name) {
   this.name = name;
 }
 //declaring products and product container
-let casizolu = new Product("../src/assets/images/casizolu.jpg", "casizolu");
-let fresina = new Product("../src/assets/images/fresina.jpg", "fresina");
-let margara2 = new Product("../src/assets/images/margara2.jpg", "margara");
-let yogurt = new Product("../src/assets/images/yogurt.jpg", "yogurt");
+let casizolu = new Product("assets/images/casizolu.jpg", "casizolu");
+let fresina = new Product("assets/images/fresina.jpg", "fresina");
+let margara2 = new Product("assets/images/margara2.jpg", "margara");
+let yogurt = new Product("assets/images/yogurt.jpg", "yogurt");
 let products = [casizolu, fresina, margara2, yogurt];
 const updatePicture = () => {
   product.setAttribute("src", products[photoCount].url);
